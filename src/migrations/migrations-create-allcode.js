112840c41004938a37a94575bd4022ec1a53800e
vinctuan02 <vinctuan02@gmail.com>
2023-10-11 01:55:19 +0700

    oke

--- src/migrations/migrations-create-allcode.js
+++ src/migrations/migrations-create-allcode.js
@@ -25,11 +25,11 @@ module.exports = {
         type: Sequelize.STRING
       },
       createdAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       },
       updatedAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       }
     });
